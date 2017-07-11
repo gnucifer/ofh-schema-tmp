3cf29eb823e63445d3f06fc5dced31cae527d275
David Gustafsson <glasklas@gmail.com>
2017-07-11 23:39:41 +0200

    Worky worky

--- schema.sql
+++ schema.sql
@@ -1,19 +1,33 @@
-DROP TABLE IF EXISTS ofh_account;
+DROP TABLE IF EXISTS ofh_user;
 -- Or account?
-CREATE TABLE ofh_account (
+CREATE TABLE ofh_user (
   id uuid PRIMARY KEY,
   username varchar(255),
   email citext UNIQUE,
-  phone varchar(50),
-  data json
+  phone varchar(50)
 -- More to come, (address, country etc)
 );
 
 DROP TABLE IF EXISTS ofh_file;
 CREATE TABLE ofh_file (
   id uuid PRIMARY KEY,
+  md5sum uuid UNIQUE,
+  name text, --varchar(255)? --filename?
   mimetype varchar(255),
-  filepath text --source??
+  data text, --Base64 encoded data
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  CHECK(EXTRACT(TIMEZONE FROM created) = '0')
+);
+-- TODO: Image styles thingy
+
+DROP TABLE IF EXISTS ofh_user_image;
+CREATE TABLE ofh_user_image (
+  user_id uuid REFERENCES ofh_user(id),
+  file_id uuid REFERENCES ofh_file(id),
+  width tinyint, --TODO: type??
+  height tinyint,
+  caption text,
+  alt text
 );
 
 -- Location table??
@@ -30,5 +44,6 @@ CREATE TABLE ofh_location (
 -- vendor?
 DROP TABLE IF EXISTS ofh_account_seller;
 CREATE TABLE ofh_account_seller (
-  id uuid PRIMARY KEY
+  id uuid PRIMARY KEY,
+  user_image_id
 );
